b495553689951c0fc7278c93509f0d02e23886ce
mai nguyen <mac@mai-nguyen.local>
2024-05-02 12:42:44 +0700

    fix face detect on android

--- src/scanFaces.ts
+++ src/scanFaces.ts
@@ -3,15 +3,15 @@ import type {
   Frame,
   FrameProcessorPlugin,
 } from './types';
-import { VisionCameraProxy } from 'react-native-vision-camera';
-import { Platform } from 'react-native';
+import {VisionCameraProxy} from 'react-native-vision-camera';
+import {Platform} from 'react-native';
 
 const plugin: FrameProcessorPlugin | undefined =
   VisionCameraProxy.initFrameProcessorPlugin('scanFaces');
 
 const LINKING_ERROR =
-  `The package 'react-native-vision-camera-v3-face-detection' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  `The package 'camera-face-detector' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 export function scanFaces(frame: Frame, options: FaceDetectionOptions): object {
